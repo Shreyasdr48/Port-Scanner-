271e1f418098994145c72cf84416c3d27f8c545b
Shreyas <workspaceshreyasdr48@gmail.com>
2024-09-26 06:22:15 +0530

    created a simple port scanner

--- Port Scanner.py
+++ Port Scanner.py
@@ -1,1 +1,11 @@
-print("Hello")
+import socket
+
+for port in range (1, 6503):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    result=sock.connect_ex (("192.168.1.1", port))
+if result ==0:
+    print("Port" + str(port) + " is open")
+    
+else:
+    print ("Port " +str(port)+ " is closed")
+sock.close()
