76f54ce8140c9d8a01e1197bafbf29906df69aa9
Shreyas <workspaceshreyasdr48@gmail.com>
2024-09-26 07:18:21 +0530

    Made it into a little more advanced project using infosec's youtube vid https://www.youtube.com/watch?v=t9EX2RAUoTU

--- Port Scanner.py
+++ Port Scanner.py
@@ -1,11 +1,46 @@
 import socket
+import subprocess
+import sys
+from datetime import datetime
 
-for port in range (1, 6503):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    result=sock.connect_ex (("192.168.1.1", port))
-if result ==0:
-    print("Port" + str(port) + " is open")
-    
-else:
-    print ("Port " +str(port)+ " is closed")
-sock.close()
+subprocess.call('clear', shell =True)
+
+remoteServer =  input("Enter a remote host to scan : ")
+remoteServerIP= socket.gethostbyname(remoteServer)
+
+print ("_" * 60 )
+
+print ("\n")
+print("░       ░░░  ░░░░  ░░░░░░░░░      ░░░       ░░░  ░░░░  ░░░      ░░░░      ░░░       ░░░  ░░░░  ░░        ░░  ░░░░░░░░  ░░░░░░░░        ░░       ░░\n▒  ▒▒▒▒  ▒▒▒  ▒▒  ▒▒▒▒▒▒▒▒▒  ▒▒▒▒  ▒▒  ▒▒▒▒  ▒▒▒  ▒▒  ▒▒▒  ▒▒▒▒▒▒▒▒  ▒▒▒▒▒▒▒▒  ▒▒▒▒  ▒▒  ▒  ▒  ▒▒  ▒▒▒▒▒▒▒▒  ▒▒▒▒▒▒▒▒  ▒▒▒▒▒▒▒▒  ▒▒▒▒▒▒▒▒  ▒▒▒▒  ▒\n▓       ▓▓▓▓▓    ▓▓▓▓▓▓▓▓▓▓  ▓▓▓▓  ▓▓       ▓▓▓▓▓    ▓▓▓▓▓      ▓▓▓▓      ▓▓▓  ▓▓▓▓  ▓▓        ▓▓      ▓▓▓▓  ▓▓▓▓▓▓▓▓  ▓▓▓▓▓▓▓▓      ▓▓▓▓       ▓▓\n█  ████  █████  ███████████        ██  ████  █████  ███████████  ████████  ██  ████  ██   ██   ██  ████████  ████████  ████████  ████████  ███  ██\n█       ██████  ███████████  ████  ██       ██████  ██████      ████      ███       ███  ████  ██        ██        ██        ██        ██  ████  █\n                                                                                                                                                   ")
+print ("\n")
+print ("_" * 60)
+print ("Please wait , scanning remote host ", remoteServerIP)
+print ("_" * 60)
+
+t1=datetime.now()
+
+try:
+    for port in range (0,30):
+        sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        result=sock.connect_ex((remoteServerIP, port))
+        if result == 0 :
+            print ("port {}:       Open".format(port))
+        sock.close()
+
+except KeyboardInterrupt:
+    print ("You pressed Ctrl+C")
+    sys.exist()
+
+except socket.gaierror:
+    print ("Host name not resolved . Exiting.........")
+    sys.exit()
+
+except socket.error():
+    print ("Socket error. Exiting.........")
+    sys.exit()
+
+t2=datetime.now()
+
+total = t2-t1
+
+print ("Total Elapsed Time : ", total)
